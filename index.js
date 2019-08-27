e9adef5b0ee0150d8004d561e3a4641d99a8c93a
Isaiah Carter <izay.carter0509@gmail.com>
2019-08-27 11:10:40 -0400

    added border-box for universal

---
+++ index.js
@@ -0,0 +1,11 @@
+function changepic()
+            {
+                if (image.getAttribute('src') == "meme-1.jpg")
+                {
+                    image.src = "meme-1.jpg";
+                }
+                else
+                {
+                    image.src = "meme-2.jpeg";
+                }
+            }
